2a9539c638e3d82b8f66ef6a1c7b678bf2ff5319
Mihails Strasuns <ms@dlangsoft.com>
2019-02-01 14:31:06 +0200

    Ignore CRLF when calling diff in tests

--- ci/test-samples.sh
+++ ci/test-samples.sh
@@ -14,7 +14,7 @@ do
     echo "Testing '$sample'"
     path="samples/$(basename "$sample" .d)"
     ./dtoh -I./samples/ -o "$path.generated" $sample
-    diff "$path.generated" "$path.expected"
+    diff --strip-trailing-cr "$path.generated" "$path.expected"
 done
 
 for sample in samples_errors/*.d
@@ -24,7 +24,7 @@ do
     set +e
     ./dtoh $sample 2>"$path.stderr"
     set -e
-    diff "$path.stderr" "$path.expected"
+    diff --strip-trailing-cr "$path.stderr" "$path.expected"
 done
 
 echo "Done.\n"
